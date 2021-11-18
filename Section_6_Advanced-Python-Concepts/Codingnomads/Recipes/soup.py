c9551c37d39a5c893eae9e0e212a80cfa48da135
Samuel Miller <90416677+SamuelMiller413@users.noreply.github.com>
2021-11-18 11:37:00 -0800

    Add files via upload

---
+++ Section_6_Advanced-Python-Concepts/Codingnomads/Recipes/soup.py
@@ -0,0 +1,11 @@
+# soup.py
+# from ingredients import carrot
+import ingredients
+
+def make_soup(ingredient):
+    soup = f"{ingredient} soup"
+    print(f"You've made {soup}")
+    return soup
+
+# print(carrot)
+# print(potato)
